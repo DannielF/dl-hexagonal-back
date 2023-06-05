b1f8c029f96ce8032acb59c7212c11aa6ac7af17
daniel.granados <daniel.granados@sofka.com.co>
2023-06-05 17:47:25 -0500

    upd: wallet controller

---
+++ src/infrastructure/http-server/controllers/wallet.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Inject, Post, UseFilters } from '@nestjs/common';
+import { WalletFilter } from '../exception-filters/wallet-exception.filter';
+import { WalletApplication } from 'src/core/application';
+import { WALLET_APPLICATION } from 'src/core/core.module';
+import { AppResponse, CreateClientRequest } from '../models';
+import { Log } from 'src/infrastructure/shared';
+
+@Controller('wallet')
+@UseFilters(WalletFilter)
+export class WalletController {
+  constructor(
+    @Inject(WALLET_APPLICATION) private application: WalletApplication,
+  ) {}
+
+  @Post()
+  async createWallet(
+    @Body() request: CreateClientRequest,
+  ): Promise<AppResponse> {
+    Log.info('Creating wallet', request);
+    const response = await this.application.createClient(request);
+
+    return {
+      status: 201,
+      message: 'Wallet created successfully',
+      data: response,
+    };
+  }
+}
