748c988bf8db3e17bdcbe7c1f78bae95aa20b176
daniel.granados <daniel.granados@sofka.com.co>
2023-06-05 15:33:25 -0500

    add: postgres docker and infra module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, InfrastructureModule],
   controllers: [],
   providers: [],
 })
