df198834434ab114b4048f15d4ac49991f1278a6
daniel.granados <daniel.granados@sofka.com.co>
2023-06-05 16:55:39 -0500

    add: client repo adapter

---
+++ src/infrastructure/adapters/ClientRepositoryAdapter.ts
@@ -0,0 +1,39 @@
+import { Client, ClientRepository } from 'src/core/domain';
+import { Repository } from 'typeorm';
+import { ClientEntity } from '../postgres';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ClientRepositoryAdapter implements ClientRepository {
+  constructor(
+    @InjectRepository(ClientEntity)
+    private repository: Repository<ClientEntity>,
+  ) {}
+
+  async findById(id: string): Promise<Client> {
+    return await this.repository.findOne({
+      where: {
+        clientId: id,
+      },
+    });
+  }
+  async findAll(): Promise<Client[]> {
+    return await this.repository.find();
+  }
+  async save(user: Client): Promise<Client> {
+    return await this.repository.save(user);
+  }
+  async update(id: string, user: Client): Promise<Client> {
+    const userToUpdate = await this.repository.preload({
+      clientId: id,
+      ...user,
+    });
+    return await this.repository.save(userToUpdate);
+  }
+  async delete(id: string): Promise<boolean> {
+    const userToDelete = await this.findById(id);
+    await this.repository.delete(userToDelete);
+    return true;
+  }
+}
--- src/infrastructure/postgres/postgres-db.module.ts
+++ src/infrastructure/postgres/postgres-db.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from '../config';
+import { ClientEntity, TransactionEntity } from './entities';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync(typeOrmAsyncConfig)],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    TypeOrmModule.forFeature([ClientEntity, TransactionEntity]),
+  ],
   exports: [TypeOrmModule],
 })
 export class PostgresDbModule {}
