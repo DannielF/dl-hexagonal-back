2ee5f3805320a007f535ee6447445c6deddcec11
daniel.granados <daniel.granados@sofka.com.co>
2023-06-05 16:59:49 -0500

    add: transaction repo adapter

---
+++ src/infrastructure/adapters/TransactionRepositoryAdapter.ts
@@ -0,0 +1,27 @@
+import { Transaction, TransactionRepository } from 'src/core/domain';
+import { Repository } from 'typeorm';
+import { TransactionEntity } from '../postgres';
+import { InjectRepository } from '@nestjs/typeorm';
+
+export class TransactionRepositoryAdapter implements TransactionRepository {
+  constructor(
+    @InjectRepository(TransactionEntity)
+    private repository: Repository<TransactionEntity>,
+  ) {}
+
+  async findById(id: string): Promise<Transaction> {
+    return await this.repository.findOne({
+      where: {
+        transactionId: id,
+      },
+    });
+  }
+
+  async findAll(): Promise<Transaction[]> {
+    return await this.repository.find();
+  }
+
+  async save(transaction: Transaction): Promise<Transaction> {
+    return await this.repository.save(transaction);
+  }
+}
