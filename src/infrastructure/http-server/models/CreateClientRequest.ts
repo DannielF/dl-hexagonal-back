a8e7c01ab87bdc0e3374046bb16b07aab1424ace
daniel.granados <daniel.granados@sofka.com.co>
2023-06-06 10:41:08 -0500

    upd: dto class-validator

--- src/infrastructure/http-server/models/CreateClientRequest.ts
+++ src/infrastructure/http-server/models/CreateClientRequest.ts
@@ -1,4 +1,11 @@
-export interface CreateClientRequest {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateClientRequest {
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- src/infrastructure/http-server/models/CreateTransactionRequest.ts
+++ src/infrastructure/http-server/models/CreateTransactionRequest.ts
@@ -1,5 +1,15 @@
-export interface CreateTransactionRequest {
+import { IsInt, IsPositive, IsString, IsUUID } from 'class-validator';
+
+export class CreateTransactionRequest {
+  @IsString()
+  @IsUUID()
   from: string;
+
+  @IsString()
+  @IsUUID()
   to: string;
+
+  @IsInt()
+  @IsPositive()
   quantity: number;
 }
